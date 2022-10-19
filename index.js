dfe0bb49407f42d435d07d72d3269ab014e94548
AmamiSQ <r_zimmerman@outlook.com>
2022-10-19 12:25:35 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//initialize params
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const { token } = require('./config.json');
+
+const prefix = '!';
+
+//create new client instance
+client.on('ready', () => {
+    console.log('pls work');
+    client.user.setActivity('Yanfei supremacy');
+});
+
+//commands
+client.on('messageCreate', (message) =>
+{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === 'ping'){    
+        message.channel.send('pong!');
+    }
+
+    if (cmd === 'shutdown'){
+        message.channel.send('shutting down...').then(() => {
+            client.destroy();
+        })
+    }
+});
+
+//logging in ( END OF FILE) //config in gitignore
+client.login(token);
