a5b94ccb0ac98e6d0278c7d9b3ecd6c90a8bda4b
AmamiSQ <r_zimmerman@outlook.com>
2022-10-25 06:08:01 +0200

    why tf doesnt js run in order ffs

--- embed.js
+++ embed.js
@@ -1,4 +1,7 @@
 const { EmbedBuilder } = require('discord.js');
+const { WebScraper } = require('./scraper.js');
+const scrape = new WebScraper();
+
 class EmbedClass
 {
     constructor()
@@ -6,7 +9,7 @@ class EmbedClass
         this.exampleEmbed;
     }
 
-    fieldBuild(asc, lvlvalue, type) //probably need to redo this one
+    fieldBuild(asc, type) //probably need to redo this one
     {
         //base fields
         let exp;
@@ -19,21 +22,31 @@ class EmbedClass
 
         this.exampleEmbed
             .addFields( 
-                { name: exp, value: lvlvalue, inline: true },
-                { name: 'Mora', value: lvlvalue, inline: true }, 
+                { name: exp, value: 'something', inline: true },
+                { name: 'Mora', value: 'something', inline: true }, 
                 { name: '\u200B', value: '\u200B' },
             );
         
-
+        
         //additional fields (depending on ascension level)
-        for(let i = 0; i<asc; i++)
-        {
-            this.exampleEmbed
-                .addFields( { name: 'Inline field title', value: 'Some value here', inline: true } );
-        };
+        if (asc == 20){
+            for(let i = 0; i<3; i++)
+            {
+                this.exampleEmbed
+                    .addFields( { name: 'yo', value: 'ffs', inline: true } );
+            };
+        }
+        else{
+            for(let i = 0; i<4; i++)
+            {
+                this.exampleEmbed
+                    .addFields( { name: 'Inline field title', value: 'Some value here', inline: true } );
+                    //.addFields( { name: name, value: matvalue, inline: true } );
+            };
+        }
     }
 
-    emBuild(title, url, description, thumbnail, image, asc, lvlvalue, type)
+    emBuild(title, url, description, thumbnail, image, asc, type)
     {
         this.exampleEmbed = new EmbedBuilder()
             .setTitle(title)
@@ -45,7 +58,8 @@ class EmbedClass
             .setTimestamp()
             .setFooter({ text: 'Source: <<addWebsite>>', iconURL: 'https://i.imgur.com/AfFp7pu.png' }); //maybe, or just maybe something fun
         
-        this.fieldBuild(asc, lvlvalue, type);
+
+        this.fieldBuild(asc, type);
 
         return this.exampleEmbed;
     }
@@ -60,9 +74,9 @@ class EmbedClass
                 { name: '\u200B', value: '\u200B' },
                 { name: '!help', value: 'Shows the command list', inline: false },
                 { name: '!character', value: 'Shows base stats and all the materials needed to level to 90 (?)', inline: false },
-                { name: '!character lvl 40', value: 'Shows materials needed to level from 30 to 40', inline: false },
+                { name: '!character 40', value: 'Shows materials needed to level from 30 to 40', inline: false },
                 { name: '!weapon', value: 'Shows base stats and all the materials needed to level to 90 (?)', inline: false },
-                { name: '!weapon lvl 50', value: 'Shows materials needed to level from 40 to 50', inline: false },
+                { name: '!weapon 50', value: 'Shows materials needed to level from 40 to 50', inline: false },
                 { name: '!ping', value: 'pong!', inline: false },
             )
             .setTimestamp()
--- index.js
+++ index.js
@@ -33,10 +33,6 @@ let asc = 3;
 let lvlvalue = 'meh';
 let type = 'char';
 
-//conditions for the fucking weapons my god
-let con1 = args[0] == 'lvl' && ascLvl.includes(args[1]);
-let con2 = args[1] == 'lvl' && ascLvl.includes(args[2]);
-let con3 = args[2] == 'lvl' && ascLvl.includes(args[3]);
 
 //create new client instance
 client.on('ready', () => {
@@ -49,11 +45,12 @@ client.on('messageCreate', (message) =>
 {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length);
+    const cmd = args.toLowerCase();
 
     if (cmd === 'ping'){    
         message.channel.send('pong!');
+
     }
 
     else if (cmd === 'shutdown'){
@@ -72,24 +69,26 @@ client.on('messageCreate', (message) =>
         message.channel.send({ embeds: [embed.helpBuild()] });
     }
 
-    else if (charArr.includes(cmd) || charArr.includes(cmd + ' ' + args[0])){
-        let character = '';
+    else if (charArr.includes(cmd)){ //use includes to check for lvl
+        let search = cmd.replace(/ /g, '-');
 
-        if (con1 || con2){
-            scrape.characterMaterials(character);
-        }
-
+        title = cmd;
         type = 'char';
-        message.channel.send(cmd + ' found');
+
+        //need to figure out how to get this in order ffs....
+        //const plswork = embed.emBuild(title, url, desc, thumb, img, 20, type); //crashes bc js FUCKING SUCKS
+        
+        //message.channel.send({ embeds: [plswork] });
+        console.log(scrape.characterMaterials(search));
     }
 
-    else if (weaponArr.includes(cmd) || weaponArr.includes(cmd + ' ' + args[0]) || weaponArr.includes(cmd + ' ' + args[0] + ' ' + args[1])){ //oh fuck me weapons like primordial WINGED jade spear exist -,-
+    else if (weaponArr.includes(cmd)){ //oh fuck me weapons like primordial WINGED jade spear exist -,-
         let weapon = '';
         let lvlasc = 0;
 
-        if (con1 || con2 || con3){
+        /* if (con1 || con2 || con3){
             scrape.weaponMaterials(weapon, lvlasc);
-        }
+        } */
 
         type = 'weapon';
         message.channel.send(cmd + ' found');
