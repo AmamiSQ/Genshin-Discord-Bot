822bed199a6128a0c1ae8c0b5f0fc977970ecdba
AmamiSQ <r_zimmerman@outlook.com>
2023-01-12 11:18:17 +0100

    code cleanup

--- index.js
+++ index.js
@@ -33,116 +33,121 @@ client.on('messageCreate', (message) =>
     const cmd = args.shift().toLowerCase();
     const second_cmd = args[0];
 
-    if (cmd === 'ping'){    
-        message.channel.send('pong!');
+    let search = cmd;
+    let lvl;
 
-    }
+    switch(true)
+    {
+        case cmd === 'ping':
+            message.channel.send('pong!');
+            break;
+        //returns a list of possible commands
+        case cmd === 'help':
+            message.channel.send({ embeds: [embed.helpBuild()] });
+            break;
+        //LMAO
+        case (charArr.includes(cmd) && args[0] === 'artifacts'):
+            message.channel.send('not yet implemented');
+            break;
+        //return characters
+        case charArr.includes(cmd):
 
-    //return list of possible commands
-    else if (cmd === 'help'){
-        message.channel.send({ embeds: [embed.helpBuild()] });
-    }
-
-    else if (charArr.includes(cmd) && args[0] === 'artifacts'){
-        message.channel.send('not yet implemented');
-    }
-
-    //return characters
-    else if (charArr.includes(cmd)){
-        let search = cmd;
-        let lvl;
-
-        if (ascLvl.includes(second_cmd)){
-            lvl = second_cmd;
-        }
-        else if (second_cmd == null){
-            lvl = '7';
-        }
-        else{
-            lvl = 'bad';
-        }
-
-        if (lvl != 'bad'){
-            scrape.searchInfo(search, 'char').then((arr) => {
-                let charInfo = arr[0];
-                let matArr = scrape.ascensionMaterials(search, lvl, 'char');
-    
-                const plswork = embed.emBuild(charInfo['title'], charInfo['url'], charInfo['description'], charInfo['image'], matArr);
-                message.channel.send({ embeds: [plswork] });
-            
-            });
-        }
-        else{
-            message.channel.send("Invalid argument, use !help to see valid commands");
-        }
-        
-        
-    }
-
-    //return weapons
-    else if (weaponArr.includes(cmd)){
-        let search = cmd;
-        let lvl;
-
-        if (ascLvl.includes(second_cmd)){
-            lvl = second_cmd;
-        }
-        else if (second_cmd == null){
-            lvl = '7';
-        }
-        else{
-            lvl = 'bad';
-        }
-
-        if (lvl != 'bad'){
-            scrape.searchInfo(search, 'weapon').then((arr) => {
-                let weaponInfo = arr[0];
-                let matArr = scrape.ascensionMaterials(search, lvl, 'weapon');
-    
-                const plswork = embed.emBuild(weaponInfo['title'], weaponInfo['url'], weaponInfo['description'], weaponInfo['image'], matArr);
-                message.channel.send({ embeds: [plswork] });
-            
-            });
-        }
-        else{
-            message.channel.send("Invalid argument, use !help to see valid commands");
-        }
-
-    }
-
-    else if (cmd === 'shutdown'){
-        message.channel.send('shutting down...').then(() => {
-            client.destroy();
-        })
-    }
-
-    else if (cmd === 'random'){
-        const accepted = ['boss', 'character'];
-
-        if (!accepted.includes(args[0])) {
-            message.channel.send("Invalid argument, use !help to see valid commands");
-
-        }
-        else{
-            let url;
-
-            if (args[0] === 'character') {
-                url = wheel.capUrl('char');
-            }
-            else {
-                url = wheel.capUrl('boss');
-
+            switch(true){
+                case ascLvl.includes(second_cmd):
+                    lvl = second_cmd;
+                    break;
+                case second_cmd == null:
+                    lvl = '7';
+                    break;
+                default: 
+                    lvl = 'bad';
+                    break;
             }
 
-            wheel.wheelSpin(url).then((result) => {
+            if (lvl != 'bad'){
+                scrape.searchInfo(search, 'char').then((arr) => {
+                    let charInfo = arr[0];
+                    let matArr = scrape.ascensionMaterials(search, lvl, 'char');
+        
+                    const plswork = embed.emBuild(charInfo['title'], charInfo['url'], charInfo['description'], charInfo['image'], matArr);
+                    message.channel.send({ embeds: [plswork] });
+                
+                });
+            }
+            else{
+                message.channel.send("Invalid argument, use !help to see valid commands");
+            }
+            break;
+        //return weapons
+        case weaponArr.includes(cmd):
 
-                const plswork = embed.spinBuild(result);
-                message.channel.send({ embeds: [plswork] });
-            });
-        }
+            switch(true){
+                case ascLvl.includes(second_cmd):
+                    lvl = second_cmd;
+                    break;
+                case second_cmd == null:
+                    lvl = '7';
+                    break;
+                default: 
+                    lvl = 'bad';
+                    break;
+            }
+
+            if (lvl != 'bad'){
+                scrape.searchInfo(search, 'weapon').then((arr) => {
+                    let weaponInfo = arr[0];
+                    let matArr = scrape.ascensionMaterials(search, lvl, 'weapon');
+        
+                    const plswork = embed.emBuild(weaponInfo['title'], weaponInfo['url'], weaponInfo['description'], weaponInfo['image'], matArr);
+                    message.channel.send({ embeds: [plswork] });
+                
+                });
+            }
+            else{
+                message.channel.send("Invalid argument, use !help to see valid commands");
+            }
+            break;
+        //returns a random character or boss
+        case cmd === 'random':
+            const accepted = ['boss', 'character'];
+
+            if (!accepted.includes(args[0])) {
+                message.channel.send("Invalid argument, use !help to see valid commands");
+
+            }
+            else{
+                let url;
+
+                if (args[0] === 'character') {
+                    url = wheel.capUrl('char');
+                }
+                else {
+                    url = wheel.capUrl('boss');
+
+                }
+
+                wheel.wheelSpin(url).then((result) => {
+
+                    const plswork = embed.spinBuild(result);
+                    message.channel.send({ embeds: [plswork] });
+                });
+            }
+            break;
+        //shutdown button
+        case cmd === 'shutdown':
+            message.channel.send('shutting down...').then(() => {
+                client.destroy();
+            })
+            break;
+        default:
+            message.channel.send("Invalid argument, use !help to see valid commands");
+            break;
+
     }
 
 });
 
 //logging in ( END OF FILE) //config in gitignore
 client.login(token);
+
+
